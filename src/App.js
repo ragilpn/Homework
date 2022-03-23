03b939ec2634c142f09f813958ec1a22b6fd34aa
Ragil PN <caturragilputrananda@gmail.com>
2022-03-23 18:14:00 +0800

    second push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+const SPOTIFY_SECRET_KEY = process.env.REACT_APP_SPOFITY_KEY;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src='https://th.bing.com/th/id/OIP.dwGjNLCuL0qmIQAGYGRiowHaHa?pid=ImgDet&w=630&h=630&rs=1'></img>
+      <h2>Sick Feeling</h2>
+      <h3>Boy Pablo</h3>
+      <p>The song details about queasiness of a breakup and reflections on the past of a now lost relationship.</p>
+      <button>Select</button>
     </div>
   );
 }
